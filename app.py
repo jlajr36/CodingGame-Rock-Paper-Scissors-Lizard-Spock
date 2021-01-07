74199f6405bb53c00663475583d13a9f48e901eb
jlajr36 <jlajr36@gmail.com>
2021-01-06 21:57:35 -0600

    Init Commit

---
+++ app.py
@@ -0,0 +1,16 @@
+import sys
+import math
+
+# Auto-generated code below aims at helping you parse
+# the standard input according to the problem statement.
+
+n = int(input())
+for i in range(n):
+    inputs = input().split()
+    numplayer = int(inputs[0])
+    signplayer = inputs[1]
+
+# Write an answer using print
+# To debug: print("Debug messages...", file=sys.stderr, flush=True)
+
+print("WHO IS THE WINNER?")
